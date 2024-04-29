ec3ffefd1edffd6f5f3f872b10c35992c6a4f080
Alejandro Di Battista <alejandrodibattista@gmail.com>
2024-04-29 00:56:17 -0300

    Generamos práctico TP1

--- practicos/30 - 58724 - Lopez Garcia, Marcos Juli/TP1/index.jsx
+++ practicos/30 - 58724 - Lopez Garcia, Marcos Juli/TP1/index.jsx
@@ -1,9 +1,8 @@
-const App = () => {
-    return (
-        <div>
-            <h1>¡Hola, mundo!</h1>
-            <p>¡Bienvenidos a React!</p>
-        </div>
-    );
-};
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () =>  (
+    <div>
+        <h1>¡Hola, mundo!</h1>
+        <p>¡Bienvenidos a React!</p>
+    </div>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
