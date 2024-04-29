1f6ce171f52c37f9055a2821e86f2035ee28ab6e
Alejandro Di Battista <alejandrodibattista@gmail.com>
2024-04-29 00:43:41 -0300

    .

---
+++ practicos/04 - 55578 - Fernandez Dumit, Patricio/TP1/index.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <div>
+            <h1>¡Hola, mundo!</h1>
+            <p>¡Bienvenidos a React!</p>
+        </div>
+    );
+};
+ReactDOM.render(<App />, document.getElementById('root'));
