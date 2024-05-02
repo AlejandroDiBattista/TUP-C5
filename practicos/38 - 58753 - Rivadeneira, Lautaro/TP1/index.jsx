0a97e527f2661bb8eb58aa47f3d8e5f07f265bc2
tarorivadenei <taroriva5199@gmail.com>
2024-05-02 12:28:12 -0300

    TP1v2

--- practicos/38 - 58753 - Rivadeneira, Lautaro/TP1/index.jsx
+++ practicos/38 - 58753 - Rivadeneira, Lautaro/TP1/index.jsx
@@ -1,23 +1,29 @@
 const ListaCompleta = [ 
-    {id : 1 , nombre: "Gabriel " , apellido: "Salazar" },
-    {id : 2 , nombre: "Ezequiel" , apellido: "Robles"},
-    {id : 3 , nombre:  "Gustavo" , apellido: "Rodriguez" }, 
+    {id : 1 , nombre: "Gabriel" , apellido: "Salazar", telefono: "3816xxxxxx"},
+    {id : 2 , nombre: "Ezequiel" , apellido: "Robles", telefono: "3816xxxxxx"},
+    {id : 3 , nombre:  "Gustavo" , apellido: "Rodriguez", telefono: "3813xxxxxx"},
+    {id : 4 , nombre:  "Belen" , apellido: "Wierna", telefono: "3816xxxxxx"},
+    {id : 5 , nombre:  "Sofia" , apellido: "Jadur", telefono: "3813xxxxxx"},
+    {id : 6 , nombre:  "Josefina" , apellido: "Rosello", telefono: "2235xxxxxx"},
+    {id : 7 , nombre:  "Lucas" , apellido: "Ruiz", telefono: "3816xxxxxx"},
 ];
 
-const Contacto = ({nombre, apellido}) => (
-    <div>
+const Contacto = ({nombre, apellido, telefono}) => (
+    <div className="contacto-card">
         <h3>Nombre: {nombre}</h3>
-        <p>Apellido: {apellido}</p>
+        <p><b>Apellido:</b> {apellido}</p>
+        <p><b>Teléfono:</b> {telefono}</p>
     </div>
 );
 
 const Agenda = () => (
-    <div>
+    <div className="agenda">
         {ListaCompleta.map((contacto) => (
             <Contacto
                 key={contacto.id}
                 nombre={contacto.nombre}
-                apellido={contacto.apellido} 
+                apellido={contacto.apellido}
+                telefono={contacto.telefono}
             />
         ))}
     </div>
@@ -28,7 +34,7 @@ const App = () => {
         <div>
             <h1>¡Hola!</h1>
             <Agenda />
-            <p>Primer Tp de React</p>
+            <p><b>Primer TP de React</b></p>
         </div>
     );
 };
