57c029875432de92aeafe8fb743804e06f68bdaf
Emiliano Stemberger <emilianostemberger123@gmail.com>
2024-05-04 22:11:09 -0300

    Avance TP1
    2do commit

--- practicos/43 - 55870 - Stemberger, Emiliano/TP1/index.jsx
+++ practicos/43 - 55870 - Stemberger, Emiliano/TP1/index.jsx
@@ -16,18 +16,20 @@ const Contacto = ({id, nombre, apellido, telefono}) =>
     </div>
 
 const App = () =>  (
-    <div>
-        <h1>¡Hola, mundo!</h1>
-        <p>¡Bienvenidos a React!</p>
-        <Contacto id = " 1 " nombre = " Emiliano " apellido = " Stemberger " telefono= " 1111 "/> 
-        <Contacto id = " 2 " nombre = " Shaquille " apellido = " O'neal " telefono= " 2222 "/> 
-        <Contacto id = " 3 " nombre = " Lionel " apellido = " Messi " telefono= " 3333 "/> 
-        <Contacto id = " 4 " nombre = " Facundo " apellido = " Campazzo " telefono= " 4444 "/> 
-        <Contacto id = " 5 " nombre = " Emanuel " apellido = " Ginobili " telefono= " 5555 "/> 
-        <Contacto id = " 6 " nombre = " Michael " apellido = " Jordan " telefono= " 6666 "/> 
-        
+    <div className="contacto">
+        <h1>Agenda</h1>
+        {contactos.map(contacto =>
+            <Contacto key = {contacto.id}
+            id={contacto.id}
+            nombre={contacto.nombre}
+            apellido={contacto.apellido}
+            telefono={contacto.telefono}        
+            />
+        )}
+            
     </div>
 )
+
 
 
 ReactDOM.render(<App/> , document.getElementById('root'))
