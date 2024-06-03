5ea81a62a07edf5f8c89b9a721b9e41be094e7a6
faustolopez11 <faustolopez1118@gmail.com>
2024-06-03 17:51:06 -0300

    tp4-LopezFausto

--- practicos/31 - 58726 - Lopez, Fausto Agustin/TP4/main.jsx
+++ practicos/31 - 58726 - Lopez, Fausto Agustin/TP4/main.jsx
@@ -1,7 +1,59 @@
+const { useState} = React
 
-const API_KEY = '30d38b26954359266708f92e1317dac0'
+const API_KEY = '93951a508fe22a447e261696e6eb30e4'
+
 function App() {
-    return <>
-        <h1>App Clima</h1>
-    </>
+    const [ciudad, setCiudad] = useState('')
+    const [datos, setDatos] = useState(null)
+
+    const buscarClima = async (city) => {
+        const datas = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`)
+        if (datas.status === 200) {
+            const data = await datas.json()
+            setDatos(data)
+        }
+    }
+
+    const cambiarCiudad = (e) => setCiudad(e.target.value) 
+
+    const unclick = (e) => {
+        const city = e.target.textContent
+        buscarClima(city)
+    }
+
+    const buscar = (e) => {
+        if(e.key === "Enter") buscarClima(ciudad) 
+    }
+
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li><h1>Clima</h1></li>
+                </ul>
+                <ul>
+                    <li><a href="#" onClick={unclick}>Tucumán</a></li>
+                    <li><a href="#" onClick={unclick}>Salta</a></li>
+                    <li><a href="#" onClick={unclick}>Buenos Aires</a></li>
+                </ul>
+            </nav>
+            <input
+                onKeyDown={buscar}
+                type="search"
+                value={ciudad}
+                onChange={cambiarCiudad}
+                placeholder="Buscar ciudad"
+            />
+
+            {datos && (<article>
+                <h2>{datos.name}</h2>
+                <div className="imagen">
+                    <img src={`./meteocons/${datos.weather[0].icon}.svg`} alt="Clima icono"></img>
+                </div>
+                <p className="temperatura">Temperatura: {datos.main.temp}°C</p>
+                <p>Mínima: {datos.main.temp_min}°C / Máxima: {datos.main.temp_max}°C</p>
+                <p>Humedad: {datos.main.humidity}%</p>
+            </article>)}
+        </>
+    )
 }
