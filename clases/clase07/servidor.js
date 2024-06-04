11bce6f57ff525a0edee61aa5fd88b0f4577c5a0
Alejandro Di Battista <alejandrodibattista@gmail.com>
2024-06-04 15:14:57 -0300

    chore: Update npm dependencies for clase07

--- clases/clase07/servidor.js
+++ clases/clase07/servidor.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import loger from 'morgan'
+import cookieParser from 'cookie-parser'
+
 import routerUsuario from './usuario.router.js'
 
 const app = express()
@@ -7,6 +9,7 @@ const app = express()
 app.use(express.static('./public'));
 app.use(express.json())
 app.use(loger('dev'))
+app.use(cookieParser())
 
 app.get('/traer', (req, res) => {
     console.log("traer en la consola del servidor")
@@ -18,10 +21,10 @@ app.post('/guardar', (req, res) => {
     console.log("REQ>", req)
     console.log("BODY>", req.body)
     res.header("X-Ouput", "Enviado en el header")
-    res.json({ mesaje: 'Ok' })
+    res.json({ mensaje: 'Ok' })
 })
 
-// app.use(routerUsuario)
+app.use(routerUsuario)
 
 app.listen(3000, () => {
     console.log("Servidor en http://localhost:3000")
