118a27724b1123ad206df2fb0e97c39ef2f1fd32
patriciofdumit <patriciofd77@gmail.com>
2024-06-05 20:16:49 -0300

    TP 5 - Patricio Fernandez

--- practicos/04 - 55578 - Fernandez Dumit, Patricio/TP5/persona.test.js
+++ practicos/04 - 55578 - Fernandez Dumit, Patricio/TP5/persona.test.js
@@ -44,95 +44,95 @@ describe("GET All", () => {
 describe("Crear una persona", () => {
 
     test("1. Crear una persona (retorna codigo)", async () => {
-        const persona = { nombre: "Juan", apellido: "Perez", edad: 30 }
-        const res = await request(app).put("/personas").send(persona)
-        expect(res.statusCode).toBe(201)
+         const persona = { nombre: "Juan", apellido: "Perez", edad: 30 }
+         const res = await request(app).put("/personas").send(persona)
+         expect(res.statusCode).toBe(201)
     })
 
     test("2. Crear una persona (y retorna su id)", async () => {
-        const persona = { nombre: "Juan", apellido: "Perez", edad: 30 }
+         const persona = { nombre: "Juan", apellido: "Perez", edad: 30 }
 
-        const res = await request(app).put("/personas").send(persona)
-        expect(res.body.id).toBeDefined()
+         const res = await request(app).put("/personas").send(persona)
+         expect(res.body.id).toBeDefined()
     })
 
     test("3. Crear una persona (debe haber uno mas)", async () => {
-        const persona = { nombre: "Juan", apellido: "Perez", edad: 30 }
+         const persona = { nombre: "Juan", apellido: "Perez", edad: 30 }
 
-        const leer = await request(app).get("/personas").send()
-        const antes = leer.body.length
+         const leer = await request(app).get("/personas").send()
+         const antes = leer.body.length
 
-        const crear = await request(app).put("/personas").send(persona)
-        expect(crear.body.id).toBeDefined()
+         const crear = await request(app).put("/personas").send(persona)
+         expect(crear.body.id).toBeDefined()
 
-        const leer2 = await request(app).get("/personas").send()
-        const despues = leer2.body.length
+         const leer2 = await request(app).get("/personas").send()
+         const despues = leer2.body.length
 
-        expect(despues).toBe(antes + 1)
+         expect(despues).toBe(antes + 1)
     })
 
-})
+ })
 
-describe("Actualizar una persona", () => {
-    test("1. Actualizar una persona (retorna codigo)", async () => {
-        const persona = { id: 2, nombre: "Juan", apellido: "Perez", edad: 30 }
-        const res = await request(app).put("/personas").send(persona)
-        expect(res.statusCode).toBe(201)
-    })
+ describe("Actualizar una persona", () => {
+     test("1. Actualizar una persona (retorna codigo)", async () => {
+         const persona = { id: 2, nombre: "Juan", apellido: "Perez", edad: 30 }
+         const res = await request(app).put("/personas").send(persona)
+         expect(res.statusCode).toBe(201)
+     })
 
-    test("2. Si no existe la persona, no se actualiza", async () => {
-        const persona = { id: 100, nombre: "Juan", apellido: "Perez", edad: 30 }
-        const res = await request(app).put("/personas").send(persona)
-        expect(res.statusCode).toBe(404)
-    })
+     test("2. Si no existe la persona, no se actualiza", async () => {
+         const persona = { id: 100, nombre: "Juan", apellido: "Perez", edad: 30 }
+         const res = await request(app).put("/personas").send(persona)
+         expect(res.statusCode).toBe(404)
+     })
 
-    test("3. Actualizar una persona (y retorna la persona actualizada)", async () => {
-        const persona = { id: 2, nombre: "JuanXX", apellido: "Perez", edad: 30 }
+     test("3. Actualizar una persona (y retorna la persona actualizada)", async () => {
+         const persona = { id: 2, nombre: "JuanXX", apellido: "Perez", edad: 30 }
 
-        const res = await request(app).put("/personas").send(persona)
-        console.log(">>>>",res.body)
-        expect(res.body.borrado).toBeDefined()
-        expect(res.body.actualizado).toBeDefined()
+         const res = await request(app).put("/personas").send(persona)
+         console.log(">>>>",res.body)
+         expect(res.body.borrado).toBeDefined()
+         expect(res.body.actualizado).toBeDefined()
 
-        expect(res.body.nombre).toBe(persona.nombre)
-    })
-})
+         expect(res.body.nombre).toBe(persona.nombre)
+     })
+ })
 
-describe("Borrar una persona", () => {
-    test("1. Borrar una persona (retorna codigo)", async () => {
-        const persona = { id: 2 }
-        const res = await request(app).put("/personas").send(persona)
-        expect(res.statusCode).toBe(201)
-    })
+ describe("Borrar una persona", () => {
+     test("1. Borrar una persona (retorna codigo)", async () => {
+         const persona = { id: 2 }
+         const res = await request(app).put("/personas").send(persona)
+         expect(res.statusCode).toBe(201)
+     })
 
-    test("2. Si no existe la persona, no se borra", async () => {
-        const persona = { id: 100 }
-        const res = await request(app).put("/personas").send(persona)
-        expect(res.statusCode).toBe(404)
-    })
+     test("2. Si no existe la persona, no se borra", async () => {
+         const persona = { id: 100 }
+         const res = await request(app).put("/personas").send(persona)
+         expect(res.statusCode).toBe(404)
+     })
 
 
-    test("3. Borrar una persona (y retorna la persona borrada)", async () => {
-        const persona = { id: 3, borrado: true }
+     test("3. Borrar una persona (y retorna la persona borrada)", async () => {
+         const persona = { id: 3, borrado: true }
 
-        const res = await request(app).put("/personas").send(persona)
-        expect(res.body.borrado).toBe(true)
-        expect(res.body.actualizado).toBeDefined()
-        expect(res.body.id).toBe(persona.id)
-    })
+         const res = await request(app).put("/personas").send(persona)
+         expect(res.body.borrado).toBe(true)
+         expect(res.body.actualizado).toBeDefined()
+         expect(res.body.id).toBe(persona.id)
+     })
 
-    test("4. Borrar una persona (debe haber uno menos)", async () => {
-        const persona = { id: 4, borrado: true }
+     test("4. Borrar una persona (debe haber uno menos)", async () => {
+         const persona = { id: 4, borrado: true }
 
-        const leerAntes = await request(app).get("/personas").send()
-        const antes = leerAntes.body.length
+         const leerAntes = await request(app).get("/personas").send()
+         const antes = leerAntes.body.length
 
-        const borrar = await request(app).put("/personas").send(persona)
-        expect(borrar.body.id).toBe(persona.id)
+         const borrar = await request(app).put("/personas").send(persona)
+         expect(borrar.body.id).toBe(persona.id)
 
-        const leerDespues = await request(app).get("/personas").send()
-        const despues = leerDespues.body.length
+         const leerDespues = await request(app).get("/personas").send()
+         const despues = leerDespues.body.length
 
-        expect(despues).toBe(antes - 1)
-    })
-})
+         expect(despues).toBe(antes - 1)
+     })
+ })
