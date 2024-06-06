3a29e09e054d37d1704ecc23cd56ad1913ed821d
Maxigarciag <maximogarciag19@gmail.com>
2024-06-06 17:26:36 -0300

    update 2

--- practicos/10 - 59154 - Garcia, Máximo/TP5/app.js
+++ practicos/10 - 59154 - Garcia, Máximo/TP5/app.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const app = express();
 
@@ -7,48 +7,83 @@ app.use(express.json());
 app.use(cors());
 
 let personas = [
-    { id: 1, nombre: "Leonel", apellido: "Mesi", edad: 36, borrado: false, actualizado: Date.now() },
-    { id: 2, nombre: "Cristian", apellido: "Ronal", edad: 38, borrado: false, actualizado: Date.now() },
-    { id: 3, nombre: "Ney", apellido: "Junior", edad: 32, borrado: false, actualizado: Date.now() },
-    { id: 4, nombre: "Kilian", apellido: "Mbapa", edad: 25, borrado: false, actualizado: Date.now() },
-    { id: 5, nombre: "Luis", apellido: "Suero", edad: 37, borrado: false, actualizado: Date.now() },
+  {
+    id: 1,
+    nombre: "Lionel",
+    apellido: "Messi",
+    edad: 36,
+    borrado: false,
+    actualizado: Date.now(),
+  },
+  {
+    id: 2,
+    nombre: "Cristiano",
+    apellido: "Ronaldo",
+    edad: 38,
+    borrado: false,
+    actualizado: Date.now(),
+  },
+  {
+    id: 3,
+    nombre: "Neymar",
+    apellido: "Junior",
+    edad: 32,
+    borrado: false,
+    actualizado: Date.now(),
+  },
+  {
+    id: 4,
+    nombre: "Kylian",
+    apellido: "Mbappe",
+    edad: 25,
+    borrado: false,
+    actualizado: Date.now(),
+  },
+  {
+    id: 5,
+    nombre: "Luis",
+    apellido: "Suarez",
+    edad: 37,
+    borrado: false,
+    actualizado: Date.now(),
+  },
 ];
 
-app.get('/personas', (req, res) => {
-    const personasinborrar = personas.filter(persona => !persona.borrado);
-    res.status(200).json(personasinborrar);
+app.get("/personas", (req, res) => {
+  const personasinborrar = personas.filter((persona) => !persona.borrado);
+  res.status(200).json(personasinborrar);
 });
 
-app.put('/personas', (req, res) => {
-    const { id, nombre, apellido, edad, borrado } = req.body;
+app.put("/personas", (req, res) => {
+  const { id, nombre, apellido, edad, borrado } = req.body;
 
-    if (id) {
-        const persona = personas.find(p => p.id === id);
-        if (persona) {
-            if (borrado !== undefined) {
-                persona.borrado = borrado;
-            } else {
-                persona.nombre = nombre;
-                persona.apellido = apellido;
-                persona.edad = edad;
-            }
-            persona.actualizado = Date.now();
-            res.status(201).json(persona);
-        } else {
-            res.status(404).send('Persona no encontrada');
-        }
+  if (id) {
+    const persona = personas.find((p) => p.id === id);
+    if (persona) {
+      if (borrado !== undefined) {
+        persona.borrado = borrado;
+      } else {
+        persona.nombre = nombre;
+        persona.apellido = apellido;
+        persona.edad = edad;
+      }
+      persona.actualizado = Date.now();
+      res.status(201).json(persona);
     } else {
-        const npersona = {
-            id: personas.length ? personas[personas.length - 1].id + 1 : 1,
-            nombre,
-            apellido,
-            edad,
-            borrado: false,
-            actualizado: Date.now()
-        };
-        personas.push(npersona);
-        res.status(201).json({ id: npersona.id });
+      res.status(404).send("Persona no encontrada");
     }
+  } else {
+    const npersona = {
+      id: personas.length ? personas[personas.length - 1].id + 1 : 1,
+      nombre,
+      apellido,
+      edad,
+      borrado: false,
+      actualizado: Date.now(),
+    };
+    personas.push(npersona);
+    res.status(201).json({ id: npersona.id });
+  }
 });
 
 export default app;
