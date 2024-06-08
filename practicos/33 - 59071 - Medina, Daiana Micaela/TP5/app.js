13fb9540b13e50c88c27d586d925eaf278318719
Micaelamedina23 <143954998+Micaelamedina23@users.noreply.github.com>
2024-06-08 16:30:57 -0300

    Tp5
    Medina Micaela 59071

--- practicos/33 - 59071 - Medina, Daiana Micaela/TP5/app.js
+++ practicos/33 - 59071 - Medina, Daiana Micaela/TP5/app.js
@@ -7,15 +7,37 @@ app.use(express.json());
 app.use(cors())
 
 let datos = [
-    // Datos de ejemplo   
+    { id: 1, nombre:'Rocio', apellido:'Juarez',edad: 29, borrado: false, actualizado: Date.now()},
+    { id: 2, nombre:'Lucas', apellido: 'Lazarte', edad: 25, borrado: false, actualizado: Date.now()},
+    { id: 3, nombre:'Valeria', apellido: 'Toledo', edad: 30, borrado: false, actualizado: Date.now()},
+    { id: 4, nombre:'Ezequiel', apellido: 'Villarroel', edad: 27, borrado: false , actualizado: Date.now()},
+    { id: 5, nombre:'Lisandro', apellido: 'Martinez', edad: 24, borrado: false , actualizado: Date.now()},
 ]
 
 app.get('/personas', (req, res) => {
-    // Implementar GET_ALL
+    const personas = datos.filter(persona =>! persona.borrado)
+    res.status(200).json(personas)
 });
 
 app.put('/personas', (req, res) => {
-    // Implementar PUT
+    const persona = req.body 
+    if (persona.id){
+        const index = datos.findIndex(p => p.id === persona.id)
+       if (index === -1){
+        res.status(404).send('Persona no encontrada')
+       } else {
+        datos[index]= {...datos[index], ...persona, actualizado: Date.now()}
+        res.status(201).json(datos[index])
+       }
+    } else {
+        const nuevaPersona = { id: datos.length ? datos[datos.length -1].id + 1 : 1,
+            ...persona,
+            borrado: false,
+            actualizado: Date.now()
+        }
+         datos.push(nuevaPersona)
+         res.status(201).json({ id: nuevaPersona.id })
+    }
 })
 
 export default app
