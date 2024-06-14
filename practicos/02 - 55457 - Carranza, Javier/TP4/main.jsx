ef7865beea0ed9c516702f295d3eb447cf4a4c42
Javier Carranza <javicarranza54@gmail.com>
2024-06-14 01:32:52 -0300

    Clima

--- practicos/02 - 55457 - Carranza, Javier/TP4/main.jsx
+++ practicos/02 - 55457 - Carranza, Javier/TP4/main.jsx
@@ -1,7 +1,91 @@
 
 const API_KEY = '30d38b26954359266708f92e1317dac0'
-function App() {
-    return <>
-        <h1>App Clima</h1>
-    </>
+const App = () => {
+    const [weatherInfo, setWeatherInfo] = useState(null);
+    const [currentCity, setCurrentCity] = useState("");
+    const [inputValue, setInputValue] = useState("");
+    const cityNames = {
+        tucuman: 'Tucumán',
+        salta: 'Salta',
+        buenosaires: 'Buenos Aires',
+    };
+    const getWeatherData = async (city) => {
+        try {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+            if (!response.ok) {
+                throw new Error(`Error: ${response.statusText}`);
+            }
+            const data = await response.json();
+            setWeatherInfo(data);
+        } catch (error) {
+            console.error("Error fetching weather data: ", error);
+        }
+    };
+    useEffect(() => {
+        if (currentCity) {
+            getWeatherData(currentCity);
+        }
+    }, [currentCity]);
+    const selectCity = (city) => {
+        setCurrentCity(city);
+    };
+    const updateInputValue = (event) => {
+        setInputValue(event.target.value);
+    };
+    const executeSearch = () => {
+        setCurrentCity(inputValue);
+        setInputValue("");
+    };
+    const onKeyDown = (event) => {
+        if (event.key === "Enter") {
+            executeSearch();
+        }
+    };
+    return (
+        <>
+            <nav>
+                <div className="titulo">
+                    <ul>
+                        <li><h1>Clima</h1></li>
+                    </ul>
+                </div>
+
+                <div className="ciudades">
+                    <ul>
+                        <li><a href="#" onClick={() => selectCity(cityNames.tucuman)}>Tucumán</a></li>
+                        <li><a href="#" onClick={() => selectCity(cityNames.salta)}>Salta</a></li>
+                        <li><a href="#" onClick={() => selectCity(cityNames.buenosaires)}>Buenos Aires</a></li>
+                    </ul>
+                </div>
+            </nav>
+
+            <div className="buscador">
+                <input
+                    type="Search"
+                    placeholder="Buscar"
+                    value={inputValue}
+                    onChange={updateInputValue}
+                    onKeyDown={onKeyDown}
+                />
+            </div>
+
+            <section>
+                {weatherInfo && (
+                    <div className="card">
+                        <h2>{weatherInfo.name}</h2>
+                        <hr />
+                        <img src={`./img/${weatherInfo.weather[0].icon}.svg`} alt="icono clima" />
+                        <hr />
+                        <div className="temperatura">
+                            <h3>Temperatura: {weatherInfo.main.temp}°C</h3>
+                            <div className="minima">
+                                <p>Mínima: {weatherInfo.main.temp_min}°C / Máxima: {weatherInfo.main.temp_max}°C</p>
+                                <p>Humedad: {weatherInfo.main.humidity}%</p>
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </section>
+        </>
+    );
 }
