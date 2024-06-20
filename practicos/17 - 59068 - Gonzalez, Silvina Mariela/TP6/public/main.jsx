1d1faf1ea0ac6bc9e85c331b3b4bc1eedebefcaa
silvinag4 <143971178+silvinag4@users.noreply.github.com>
2024-06-20 19:06:42 -0300

    tp6

--- practicos/17 - 59068 - Gonzalez, Silvina Mariela/TP6/public/main.jsx
+++ practicos/17 - 59068 - Gonzalez, Silvina Mariela/TP6/public/main.jsx
@@ -1,7 +1,144 @@
 function App() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [loginUsername, setLoginUsername] = useState('');
+    const [loginPassword, setLoginPassword] = useState('');
+    const [message, setMessage] = useState('');
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [userData, setUserData] = useState(null);
+    const [showRegisterForm, setShowRegisterForm] = useState(true);
+    const [showLoginForm, setShowLoginForm] = useState(false);
+
+    const url = 'http://localhost:3000';
+
+    const handleRegister = async (event) => {
+        event.preventDefault();
+        if (password !== confirmPassword) {
+            setMessage('Las contraseñas no coinciden');
+            return;
+        }
+
+        const response = await fetch(`${url}/usuarios/crear-usuario`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, contrasena: password })
+        });
+
+        const data = await response.json();
+        if (response.ok) {
+            setMessage('Has sido registrado con éxito.');
+        } else {
+            setMessage(data.error);
+        }
+    };
+
+    const handleLogin = async (event) => {
+        event.preventDefault();
+
+        const response = await fetch(`${url}/usuarios/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username: loginUsername, contrasena: loginPassword })
+        });
+
+        const data = await response.json();
+        if (response.ok) {
+            setMessage(`Sesión iniciada con éxito.`);
+            setUserData(data);
+            setLoggedIn(true);
+        } else {
+            setMessage(data.error);
+        }
+    };
+
+    const handleLogout = async () => {
+        const response = await fetch(`${url}/usuarios/logout`, {
+            method: 'POST',
+        });
+
+        const data = await response.json();
+        if (response.ok) {
+            setMessage('Sesión cerrada con éxito');
+            setLoggedIn(false);
+            setUserData(null);
+        } else {
+            setMessage(data.error);
+        }
+    };
+
     return (
-        <div>
-            <h1>TP6 - Sesiones</h1>
-        </div>
+        <>
+            <header>
+                <section className="navbar">
+                    {!loggedIn && (
+                        <>
+                            <a href="#" onClick={() => { setShowRegisterForm(true); setShowLoginForm(false); }}>REGISTRO</a>
+                            <a href="#" onClick={() => { setShowLoginForm(true); setShowRegisterForm(false); }}>INICIO DE SESIÓN</a>
+                        </>
+                    )}
+                    {loggedIn && <a href="#" onClick={handleLogout}>CERRAR SESIÓN</a>}
+                </section>
+            </header>
+            <main>
+                <section>
+                    {!loggedIn && showRegisterForm && (
+                        <form onSubmit={handleRegister}>
+                            <input
+                                type="text"
+                                placeholder="Nombre de usuario"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
+                            <input
+                                type="password"
+                                placeholder="Contraseña"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <input
+                                type="password"
+                                placeholder="Confirmar contraseña"
+                                value={confirmPassword}
+                                onChange={(e) => setConfirmPassword(e.target.value)}
+                            />
+                            <button type="submit" className="submit">REGISTRAR</button>
+                        </form>
+                    )}
+                    {!loggedIn && showLoginForm && (
+                        <form onSubmit={handleLogin}>
+                            <input
+                                type="text"
+                                placeholder="Nombre de usuario"
+                                value={loginUsername}
+                                onChange={(e) => setLoginUsername(e.target.value)}
+                            />
+                            <input
+                                type="password"
+                                placeholder="Contraseña"
+                                value={loginPassword}
+                                onChange={(e) => setLoginPassword(e.target.value)}
+                            />
+                            <button type="submit" className="submit">INICIAR SESIÓN</button>
+                        </form>
+                    )}
+                    {loggedIn && (
+                        <section className="mensaje">
+                            <h1>Bienvenido, {userData.username}</h1>
+                        </section>
+                    )}
+                    <section className="mensaje">
+                        <h1>{message}</h1>
+                    </section>
+                </section>
+            </main>
+            <footer>
+            </footer>
+
+        </>
     )
 }
--- practicos/17 - 59068 - Gonzalez, Silvina Mariela/TP6/servidor.js
+++ practicos/17 - 59068 - Gonzalez, Silvina Mariela/TP6/servidor.js
@@ -1,13 +1,90 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import morgan
- from 'morgan';
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import cors from 'cors';
+import session from 'express-session';
+
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(session({
+    secret: 'mi_secreto',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false }
+}));
+
+app.use(cors());
 app.use(morgan('dev'));     // Loggea cada request en consola
 app.use(cookieParser());    // Para leer cookies
 app.use(express.json());    // Para leer JSONs
-app.use(express.static('public'));  // Para servir archivos estáticos
+const pathJoin = path.join(__dirname, 'public')
+app.use(express.static(pathJoin));  // Para servir archivos estáticos
+
+let usuarios = [
+    { id: 1, username: 'Silvina', contrasena: '123' },
+    { id: 2, username: 'Ulises', contrasena: '123' },
+    { id: 3, username: 'Eli', contrasena: '123' },
+    { id: 4, username: 'Mariela', contrasena: '123' },
+    { id: 5, username: 'Sergio', contrasena: '123' },
+];
+
+app.get('/usuarios/obtener-usuarios', (req, res) => {
+    res.status(200).json(usuarios);
+});
+
+app.post('/usuarios/crear-usuario', (req, res) => {
+    const { username, contrasena } = req.body;
+
+    if (!username || !contrasena) {
+        return res.status(400).json({ error: 'Datos de usuario no válidos.' });
+    }
+
+    let usuarioExistente = false;
+
+    usuarios.forEach((u, index) => {
+        if (u.username === username) {
+            usuarios[index] = { ...u, contrasena };
+            usuarioExistente = true;
+        }
+    });
+
+    if (usuarioExistente) {
+        res.status(409).json({ error: 'El usuario ya existe.' });
+    } else {
+        usuarios.push({ id: usuarios.length + 1, username, contrasena });
+        res.status(201).json({ username });
+    }
+});
+
+app.post('/usuarios/login', (req, res) => {
+    const { username, contrasena } = req.body;
+
+    if (!username || !contrasena) {
+        return res.status(400).json({ error: 'Datos de usuario no válidos.' });
+    }
+
+    const usuario = usuarios.find(u => u.username === username && u.contrasena === contrasena);
+
+    if (usuario) {
+        res.status(200).json({ message: 'Inicio de sesión exitoso', username: usuario.username });
+    } else {
+        res.status(401).json({ error: 'Credenciales no válidas' });
+    }
+});
+
+app.post('/usuarios/logout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            return res.status(500).json({ error: 'No se pudo cerrar la sesión' });
+        }
+        res.status(200).json({ message: 'Sesión cerrada con éxito' });
+    });
+});
 
 // Implementar las rutas necesarias
 app.listen(3000, () => {
