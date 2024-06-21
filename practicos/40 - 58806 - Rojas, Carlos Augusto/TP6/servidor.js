bcdffa62b38aa402d8d8c8d82738e69487bc94eb
AugusRojas <augus32132@gmail.com>
2024-06-21 18:52:59 -0300

    Terminado

--- practicos/40 - 58806 - Rojas, Carlos Augusto/TP6/servidor.js
+++ practicos/40 - 58806 - Rojas, Carlos Augusto/TP6/servidor.js
@@ -1,25 +1,15 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import morgan from "morgan";
-import controller from "./controller.js";
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import morgan
+ from 'morgan';
 const app = express();
 
-app.use(morgan("dev")); // Loggea cada request en consola
-app.use(cookieParser()); // Para leer cookies
-app.use(express.json()); // Para leer JSONs
-app.use(express.static("public")); // Para servir archivos estáticos
+app.use(morgan('dev'));     // Loggea cada request en consola
+app.use(cookieParser());    // Para leer cookies
+app.use(express.json());    // Para leer JSONs
+app.use(express.static('public'));  // Para servir archivos estáticos
 
 // Implementar las rutas necesarias
-app.get("/validation", controller.validation);
-
-app.put("/login", controller.login);
-
-app.put("/logout", controller.logout);
-
-app.post("/register", controller.register);
-
-app.get("/mostrarDatos", controller.mostrarDatos);
-
 app.listen(3000, () => {
-  console.log("Server runnin in http://localhost:3000");
+    console.log('Servidor iniciado en http://localhost:3000');
 });
