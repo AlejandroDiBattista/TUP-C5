92af006f6277cbea90e04374c3efa5a8f9c0793d
Augusto <augustoterrera@gmail.com>
2024-06-21 20:55:25 -0300

    TP6-TerreraAugusto

--- practicos/44 - 58845 - Terrera, Augusto Dante/TP6/public/main.jsx
+++ practicos/44 - 58845 - Terrera, Augusto Dante/TP6/public/main.jsx
@@ -1,7 +1,166 @@
+const { useState } = React
+
+const Login = ({ onLogin }) => {
+    const [user, setUser] = useState('')
+    const [password, setPassword] = useState('')
+    const [mostrar, setMostrar] = useState(false)
+
+
+    const ingresar = async (e) => {
+        e.preventDefault()
+        if (!user || !password) {
+            alert('Todos los campos son obligatorios')
+            return
+        }
+        else {
+            try {
+                const res = await fetch('http://localhost:3000/login', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                        user: user,
+                        password: password
+                    })
+                })
+                const data = await res.json();
+                if (data) {
+                    onLogin(data);
+                } else {
+                    alert('Usuario o contraseña incorrecta');
+                }
+            } catch (error) {
+                alert('Error al conectar con el servidor')
+            }
+        }
+    }
+
+    const registrar = (e) => {
+        e.preventDefault()
+        setMostrar(true)
+    }
+
+
+    return (
+        <>
+            {mostrar
+                ?
+                <Registrar />
+                :
+                <div>
+                    <h1>Login</h1>
+                    <form>
+                        <input type="text" placeholder="Username" onChange={(e) => setUser(e.target.value)} />
+                        <br />
+                        <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+                        <br />
+                        <button onClick={ingresar}>Ingresar</button>
+                        <button onClick={registrar}>Registrar</button>
+                    </form>
+                </div>
+            }
+        </>
+    )
+}
+
+const Registrar = () => {
+    const [user, setUser] = useState('')
+    const [password, setPassword] = useState('')
+    const [rPassword, setRPassword] = useState('')
+    const [mostrar, setMostrar] = useState(false)
+
+    const registrarse = async (e) => {
+        e.preventDefault()
+        if (!user || !password ) {
+            alert('Todos los campos son necesarios!')
+            return
+        }
+        else if (password.length < 4 || rPassword.length < 4) {
+            alert('La contraseña debe tener al menos 8 caracteres')
+            return
+        }
+        else if (rPassword != password) {
+            alert('Las contraseñas no coinciden')
+            return
+        }
+        else {
+            try {
+                const res = await fetch('http://localhost:3000/register', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ user, password })
+                });
+                const data = await res.json()
+                if (res.status === 201) {
+                    alert('Usuario registrado con éxito')
+                    setMostrar(true)
+                } else {
+                    alert('Error al registrar usuario: ')
+                }
+            } catch (error) {
+                alert('Error al conectar con el servidor')
+            }
+        }
+    }
+
+    return (
+        <>
+            {mostrar
+                ?
+                <Login />
+                :
+                <div>
+                    <h1>Registrar</h1>
+                    <form>
+                        <input type="text" placeholder="Usuario" onChange={(e) => setUser(e.target.value)} />
+                        <br />
+                        <input type="password" placeholder="Contraseña" onChange={(e) => setPassword(e.target.value)} />
+                        <br />
+                        <input type="password" placeholder="Repetir contraseña" onChange={(e) => setRPassword(e.target.value)} />
+                        <br />
+                        <button onClick={registrarse}>Registrarse</button>
+                    </form>
+                </div>
+            }
+        </>
+    )
+}
+
 function App() {
+    const [authenticated, setAuthenticated] = useState(false)
+    const [user, setUser] = useState(null)
+
+    const handleLogin = (userData) => {
+        setUser(userData)
+        setAuthenticated(true)
+    }
+    const logout = async () => {
+        try {
+            const res = await fetch('http://localhost:3000/logout', {
+                method: 'PUT',
+                credentials: 'include'
+            });
+            if (res.ok) {
+                setUser(null);
+                setAuthenticated(false);
+                alert('Usuario deslogueado');
+            } else {
+                alert('Error al cerrar sesión');
+            }
+        } catch (error) {
+            alert('Error al conectar con el servidor');
+        }
+    };
+
     return (
         <div>
-            <h1>TP6 - Sesiones</h1>
+            {authenticated
+                ? <div>
+                    <h1>Bienvenido, {user.user}</h1>
+                    <p>Token: {user.token}</p>
+                    <button onClick={logout}>Cerrar Sesion</button>
+                </div>
+                : <Login onLogin={handleLogin} />
+            }
         </div>
     )
 }
--- practicos/44 - 58845 - Terrera, Augusto Dante/TP6/servidor.js
+++ practicos/44 - 58845 - Terrera, Augusto Dante/TP6/servidor.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import morgan
- from 'morgan';
+import morgan from 'morgan';
+import { login, register, logout, validarUsuario, getInfo } from './controllers/controlador.js';
+
 const app = express();
 
 app.use(morgan('dev'));     // Loggea cada request en consola
@@ -9,7 +10,16 @@ app.use(cookieParser());    // Para leer cookies
 app.use(express.json());    // Para leer JSONs
 app.use(express.static('public'));  // Para servir archivos estáticos
 
+
 // Implementar las rutas necesarias
+
+app.post('/login', login)
+app.post('/register', register)
+app.put('/logout', validarUsuario, logout)
+app.get('/info', validarUsuario, getInfo);
+
+
+
 app.listen(3000, () => {
     console.log('Servidor iniciado en http://localhost:3000');
 });
