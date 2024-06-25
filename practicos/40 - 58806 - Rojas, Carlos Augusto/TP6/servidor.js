0dc6e689bee0912903504069efbde39b72ce1f22
AugusRojas <augus32132@gmail.com>
2024-06-25 08:14:17 -0300

    enviado correctamente

--- practicos/40 - 58806 - Rojas, Carlos Augusto/TP6/servidor.js
+++ practicos/40 - 58806 - Rojas, Carlos Augusto/TP6/servidor.js
@@ -1,3 +1,4 @@
+import controller from './controller.js';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import morgan
@@ -10,6 +11,16 @@ app.use(express.json());    // Para leer JSONs
 app.use(express.static('public'));  // Para servir archivos estáticos
 
 // Implementar las rutas necesarias
+app.get("/validation", controller.validation);
+
+app.put("/login", controller.login);
+
+app.put("/logout", controller.logout);
+
+app.post("/register", controller.register);
+
+app.get("/mostrarDatos", controller.mostrarDatos);
+
 app.listen(3000, () => {
     console.log('Servidor iniciado en http://localhost:3000');
 });
