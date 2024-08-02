5b9d19e22743d6400d03e81c3cabf65a240ed601
Alejandro Di Battista <alejandrodibattista@gmail.com>
2024-08-02 01:32:44 -0300

    Update actualizar.js

--- actualizar.js
+++ actualizar.js
@@ -1,19 +1,11 @@
-function subirCondicionC5() {
+function cambiarCondicionC5() {
     let datos = [4, 4, 2, 2, 2, 2, 4, 2, 2, 2, 4, 4, 2, 4, 2, 4, 4, 4, 2, 2, 4, 4, 4, 4, 2, 2, 4, 4, 2, 2, 2, 2, 2, 4, 4, 4, 4, 4, 2, 2, 4, 2, 2, 2, 4, 2, 2, 2, 4, 2]
     let estados = document.querySelectorAll('select[name="nota"]');
-    if (datos.length != estados.length) {
-        alert('Error en la cantidad de alumnos >> Alumnos: ' + datos.length + 'Condiciones: ' + estados.length);
-    } else {
-        estados.forEach((estado, index) => estado.selectedIndex = datos[index]);
-    }
+    estados.forEach((estado, index) => estado.selectedIndex = datos[index]);
 }
 
-function subirNotaC5() {
-    let datos = [10, 10, 7, 7, 7, 8, 10, 8, 7, 8, 10, 10, 8, 10, 8, 10, 10, 10, 8, 8, 10, 10, 9, 10, 8, 8, 9, 10, 8, 8, 8, 8, 8, 10, 10, 10, 10, 9, 8, 8, 10, 8, 8, 8, 10, 7, 8, 8, 9, 8]
+function cambiarNotaC5() {
+    let datos = [10, 10, 0, 0, 0, 0, 10, 0, 0, 0, 10, 10, 0, 10, 0, 10, 10, 10, 0, 0, 10, 10, 9, 10, 0, 0, 9, 10, 0, 0, 0, 0, 0, 10, 10, 10, 10, 9, 0, 0, 10, 0, 0, 0, 10, 0, 0, 0, 9, 0]
     let notas = document.querySelectorAll('input[name="nota"]');
-    if (datos.length != notas.length) {
-        alert('Error en la cantidad de alumnos >> Alumnos: ' + datos.length + 'Notas: ' + notas.length);
-    } else {
-        notas.forEach((nota, index) => nota.value = datos[index]);
-    }
+    notas.forEach((nota, index) => nota.value = datos[index]);
 }
